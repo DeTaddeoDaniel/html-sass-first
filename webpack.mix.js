7fd17caa1f3974fbba39efc13dbdf072d41f3837
Daniel2711 <detaddeo.d@gmail.com>
2021-02-19 21:18:54 +0100

    Test ok npm mix watch

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
-
+// crea variabile mix
 let mix = require('laravel-mix');
 
+// compila i file seguenti
 mix.sass('scss/app.scss', 'css');
 
+// sincronizza la pagina automaticamente ad ogni cambio
 mix.browserSync('myapp.test')
