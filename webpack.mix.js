061eb437a800c0f1ffcfd12105b9d50adeb2266e
Daniel2711 <detaddeo.d@gmail.com>
2021-02-21 09:34:22 +0100

    Compilatore js

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,8 @@ let mix = require('laravel-mix');
 // compila i file seguenti
 mix.sass('resource/scss/app.scss', 'resource/dist/css');
 
+// compila js
+mix.js('resource/js/app.js', 'resource/dist/js');
+
 // sincronizza la pagina automaticamente ad ogni cambio
 mix.browserSync('myapp.test')
